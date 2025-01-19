969c1d9dd2cfde6b28f97ca853472f41fdcd7c85
muzardemoses <muzardemoses@gmail.com>
2025-01-19 06:23:13 +0100

    seo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,35 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const envUrl = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://jokes.mosesadebayo.tech";
+
+
 export const metadata: Metadata = {
   title: "Jokes AI",
-  description: "Jokes Generator AI build by Moses Adebayo",
+  description: "Jokes AI is your personal, on-demand comedian, powered by cutting-edge artificial intelligence. With a simple input—just provide a subject or theme—Jokes AI generates witty, funny, and sometimes sarcastic jokes that are perfect for any occasion. Whether you're looking to lighten the mood, break the ice, or just share a laugh with friends, Jokes AI delivers the perfect punchline every time.",
+  openGraph: {
+    type: "website",
+    url: "https://jokes.mosesadebayo.tech",
+    title: "Jokes AI",
+    description: "Jokes AI is your personal, on-demand comedian, powered by cutting-edge artificial intelligence. With a simple input—just provide a subject or theme—Jokes AI generates witty, funny, and sometimes sarcastic jokes that are perfect for any occasion. Whether you're looking to lighten the mood, break the ice, or just share a laugh with friends, Jokes AI delivers the perfect punchline every time.",
+    images: [
+      {
+        url: `${envUrl}/Images/logo.png`,
+        width: 900,
+        height: 600,
+        alt: "Wholesquare Logo",
+      },
+    ],
+  },
+  twitter: {
+    creator: "@muzarde1",
+    site: "@muzarde1",
+    card: "summary",
+  },
+  authors: [{ name: "Moses Adebayo", url: "https://mosesadebayo.tech" }],
+  creator: "Moses Adebayo",
+  publisher: "Moses Adebayo",
+  applicationName: "Jokes AI",
 };
 
 export default function RootLayout({
@@ -24,6 +50,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
